139209bc75dee47e91d152136ad34717cfed1f6c
qiyuanchen <chenqiyuan1012@foxmail.com>
2023-10-20 17:55:38 +0800

    add css for all pages

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,85 +1,52 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <el-container>
+    <el-header>睿医网信办 V2.0</el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+
+<style>
+.el-container {
+  height: 100%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+.el-header {
+  background-color: #E5EAF3;
+  color: #409EFF;
   text-align: center;
-  margin-top: 2rem;
+  height: auto;
+  line-height: 60px;
+  font-size: x-large;
+  font-weight: 800;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.el-footer {
+  background-color: #E5EAF3;
+  color: #409EFF;
+  text-align: center;
+  line-height: 60px;
+  font-size: medium;
+  font-weight: 400;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.el-aside {
+  background-color: #E5EAF3;
+  color: #409EFF;
+  text-align: center;
+  line-height: 200px;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.el-main {
+  background-color: #E5EAF3;
+  color: #409EFF;
+  text-align: center;
+  line-height: 160px;
 }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.el-menu {
+  background-color: #E5EAF3;
+  color: #409EFF;
 }
 </style>
